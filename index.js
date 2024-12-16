ecab0910eaa1a19b0951fd1761a05f2b38aa3a59
sisu <danielcoder1022@gmail.com>
2024-12-16 16:57:06 +0900

    fix: cors

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(cors({
+  origin: 'https://design-service-znjc.vercel.app/order', // Replace with your frontend URL
+  methods: ['GET', 'POST'],       // Allow only specific HTTP methods
+  credentials: true               // Enable cookies if needed
+}));
 
 app.use(bodyParser.json());
 
