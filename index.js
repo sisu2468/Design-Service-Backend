f16573fc2ce5692c7f166d7826c531e93623b08b
sisu <danielcoder1022@gmail.com>
2024-12-16 16:48:31 +0900

    feat: design backend

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const axios = require("axios");
+const sendformnotification = require("./sendformnotification");
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.sakura.ne.jp', // さくらインターネットのSMTPサーバー
+  port: 465, // SSLを使用するポート
+  secure: true, // trueでSSLを有効化
+  auth: {
+    user: "info@bremen.co.jp",
+    pass: "aork umuw vdkn batl", // アプリパスワードを使用推奨
+  },
+});
+
+function generateOrderNumber() {
+  return Math.floor(10000 + Math.random() * 90000); // 10000〜99999の範囲でランダムな整数を生成
+}
+
+app.post("/deliver", async (req, res) => {
+  const buyername = req.body.buyername;
+  const ordernumber = generateOrderNumber(); // ランダムな注文番号
+  const emailaddress = req.body.emailaddress;
+  const postalcode = req.body.postalcode;
+  const address = req.body.address;
+  const telnumber = req.body.telnumber;
+  const orderdate = req.body.orderdate;
+  const products = req.body.products;
+  const totalprice = req.body.totalprice;
+  const deliverydate = req.body.deliverydate;
+
+  console.log("data", req.body);
+
+  // 通知メールを送信
+  const mailOptions = {
+    from: "株式会社ブレーメン",
+    to: emailaddress,
+    subject: "ブレーメンデジタルフラッグのご注文",
+    html: sendformnotification.sendformnotification(
+      buyername,
+      ordernumber,
+      emailaddress,
+      postalcode,
+      address,
+      telnumber,
+      orderdate,
+      products,
+      totalprice,
+      deliverydate
+    ),
+  };
+  await transporter.sendMail(mailOptions);
+  res.status(200).send("メールを送信しました");
+});
+
+// サーバーを開始
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`サーバーが起動しました: http://localhost:${PORT}`);
+});
---
+++ sendformnotification.js
@@ -0,0 +1,150 @@
+module.exports = {
+  sendformnotification: (buyername, ordernumber, emailaddress, postalcode, address, telnumber, orderdate, products, totalprice, deliverydate) => {
+    return `
+        <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>フォーム提出通知</title>
+        <style>
+        .content { font-family: Arial, sans-serif; }
+        .clinic { margin-bottom: 30px;}
+        .phase1 { margin-bottom: 30px;}
+        .phase2 { margin-bottom: 50px;}
+        .companyinfo {font-weight: 700;}
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <h1 class="clinic">${buyername} 様</h1>
+            <p class="phase1">この度はブレーメンデジタルフラッグのご注文いただき、誠にありがとございます。</p>
+            <p class="phase1">本日、以下のご注文を承りましたので、ご確認をお願い申し上げます。</p>
+            <p>
+                【　受　注　番　号　】${ordernumber}
+                <br>
+                ▼お客様情報
+                <br>
+                ================================
+                <br>
+                【　お　　名　　前　】${buyername} 様
+                <br>
+                【　メールアドレス　】${emailaddress}
+                <br>
+                【　郵　便　番　号　】${postalcode}
+                <br>
+                【　ご　　住　　所　】${address}
+                <br>
+                【　電　話　番　号　】${telnumber}
+                <br>
+                【　注　　文　　日　】${orderdate}
+                <br>
+                【　決　済　方　法　】銀行振込
+                <br>
+                ================================
+            </p>
+            <p>
+                ▼配送先情報
+                <br>
+                ================================
+                <br>
+                【　お　　名　　前　】
+                <br>
+                【　郵　便　番　号　】
+                <br>
+                【　ご　　住　　所　】
+                <br>
+                【　電　話　番　号　】
+                <br>
+                ================================
+                <br>
+            </p>
+            <p>
+                ▼商品詳細
+                <br>
+                ================================
+                <br>
+                ${products.map ((products, index) => `
+                  【　商　　品　　名　】${products.flagtype}
+                  <br>
+                  【　　数　　　量　　】${products.amount}
+                  <br>
+                  【　　小　　　計　　】${products.subtotal}
+                  <br>
+                  --------------------------------
+                  <br>
+                  ================================
+                  <br>
+                `).join('')}
+            </p>
+            <p>
+                ▼総合計
+                <br>
+                ================================
+                <br>
+                【　総　　合　　計　】${totalprice}
+                <br>
+                ================================
+                <br>
+            </p>
+            <p class="phase1">
+                【納品予定日】
+                <br>
+                この注文の納品予定日は、${deliverydate}日です。
+                <br>
+            </p>
+            <p class="phase2">
+                【振込先口座情報】
+                <br>
+                ーーーーーーーーーーーー
+                <br>
+                金融機関名：PayPay銀行
+                <br>
+                支店名：すずめ支店(002)
+                <br>
+                口座番号：3747255
+                <br>
+                口座名義：カ）ブレーメン
+                <br>
+                ーーーーーーーーーーーー
+                <br>
+                ※振込手数料はお客様ご負担でお願いいたします。
+                <br>
+                ※ご入金の確認後の手配となります。
+                <br>
+            </p>
+            <p class="phase2">
+                【今後の流れについて】
+                <br>
+                商品はお支払い完了後の手配となります。
+                <br>
+                お客様からのご入金が確認できない場合、次の納期スケジュールとなりますこと、
+                <br>
+                あらかじめご了承ください。
+                <br>
+                詳しくは納品スケジュールをご確認いただけますようお願い申し上げます。
+                <br>
+            </p>
+            <p class="phase2">
+                株式会社ブレーメン───────────
+                <br>
+                〒662-0045
+                <br>
+                兵庫県西宮市安井町2-33
+                <br>
+                TEL：(0798)33-4737(代)
+                <br>
+                HP：http://www.bremen.co.jp/
+                <br>
+                MAIL：info@bremen.co.jp
+                <br>
+                営業時間：9:30 - 17:00（土・日・祝日休み）
+                <br> ────────────────────
+                <br>
+                <br>
+            </p>
+        </div>
+    </body>
+</html>
+    `
+  }
+}
